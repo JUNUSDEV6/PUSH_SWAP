2f8d2a10d9d9ed67be5fad458266fdfb3e6398fe
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2023-10-19 13:31:38 +0200

    4

--- init_stack.c
+++ init_stack.c
@@ -6,7 +6,7 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:29:44 by yohanafi          #+#    #+#             */
-/*   Updated: 2023/10/19 13:25:13 by yohanafi         ###   ########.fr       */
+/*   Updated: 2023/10/19 13:31:10 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	init_stack(t_stack_node **a, char **argv, bool flag)
 {
-	
+	long	nbr;
+
+	while (*argv)
+	{
+		nbr = ft_atol(*argv);
+	}
 }
