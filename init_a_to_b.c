6f86a0542a73fd4fd358a746e1821620b00a9736
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2023-11-15 17:01:45 +0100

    6

--- init_a_to_b.c
+++ init_a_to_b.c
@@ -6,7 +6,7 @@
 /*   By: youneshanafi <youneshanafi@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 17:36:38 by youneshanaf       #+#    #+#             */
-/*   Updated: 2023/11/15 16:22:15 by youneshanaf      ###   ########.fr       */
+/*   Updated: 2023/11/15 17:01:27 by youneshanaf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,37 @@ void	current_index(t_stack_node *a)
 		stack = stack->next;
 		i++;
 	}
+}
+
+void	target_smaller(t_stack_node *a, t_stack_node *b)
+{
+	t_stack_node	*current_b;
+	t_stack_node	*target_node;
+	long			best_match_index;
+
+	while (a)
+	{
+		current_b = b;
+		best_match_index = LONG_MIN;
+		while (current_b)
+		{
+			if (current_b->nbr < a->nbr && current_b->nbr > best_match_index)
+			{
+				target_node = current_b;
+				best_match_index = current_b->nbr;
+			}
+			current_b = current_b->next;
+		}
+		if (best_match_index == LONG_MIN)
+			a->target_node = find_big(a);
+		else
+			a->target_node = target_node;
+		a = a->next;
+	}
+}
+void	cost_operation(t_stack_node *a, t_stack_node *b)
+{
+	
 }
 
 void	algo_a_to_b(t_stack_node *a, t_stack_node *b)
